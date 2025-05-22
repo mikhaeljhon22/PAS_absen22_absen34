f4232ee59ae3adda3cccf96c3c8fa5e31350159f
mikhaeljhon22 <mikhaeljhon22@gmail.com>
2025-05-22 09:37:38 +0700

    added bottom nav menu

---
+++ app/src/main/java/com/club/pas_absen22_absen34/Biodaata.java
@@ -0,0 +1,16 @@
+package com.club.pas_absen22_absen34;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Biodaata extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_biodata, container, false);
+        return view;
+    }
+}
